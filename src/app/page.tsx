5501a74313565e9093207456503f17397aab9e1e
RAIYAN2024 <raiyansiddik2022@gmail.com>
2025-01-31 02:45:19 +0600

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Banner from "@/components/Banner";
+import Footer from "@/components/Fotter";
 import Image from "next/image";
 
 export default async function App() {
@@ -8,7 +9,7 @@ export default async function App() {
     <>
       <Banner />
       <div className="container mx-auto mt-20 p-2 xl:px-36">
-        <div className="grid 2xl:grid-cols-6 md:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-2">
+        <div className="grid 2xl:grid-cols-6 md:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-2 mb-10">
           {data?.map((item: Product) => (
             <div className="shadow-[0 0 10 #f5f5f5] bg-white" key={item.id}>
               <Image
@@ -29,6 +30,7 @@ export default async function App() {
           ))}
         </div>
       </div>
+      <Footer />
     </>
   );
 }
